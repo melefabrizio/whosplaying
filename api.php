c3ded4a00c15b6ce910f93b40eef3eaf2b9e35bb
Fabrizio Mele <fabriziomele92@gmail.com>
2016-02-19 14:06:07 +0100

    n-esimo tentativo

--- api.php
+++ api.php
@@ -1,11 +1,12 @@
 <?php
-define('__ROOT__', dirname(__FILE__)); 
+define('__ROOT__', __DIR__); 
 require_once  __ROOT__.'/meekrodb.2.2.class.php';
 require_once  __ROOT__.'/conf.php';
 require_once  __ROOT__.'/Rito.php';
 require_once  __ROOT__.'/APIClass.class.php';
 require_once  __ROOT__.'/ConcreteApi.php';
 require_once  __ROOT__.'/class.Model.php';
+
 
 global  $model;
 $model = new Modello();
