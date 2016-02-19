96f5d17aa091b729942d9f706a96c395bca8e5a5
Fabrizio Mele <fabriziomele92@gmail.com>
2016-02-19 11:43:30 +0100

    aggiunto webook "helloworld" per il bot

---
+++ webhook.php
@@ -0,0 +1,20 @@
+<?php 
+require_once 'conf.php';
+// read incoming info and grab the chatID
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+$chatID = $update["message"]["chat"]["id"];
+		
+// compose reply
+$reply =  sendMessage();
+		
+// send reply
+$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
+file_get_contents($sendto);
+
+function sendMessage(){
+$message = "I am a baby bot.";
+return $message;
+}
+
+?>
