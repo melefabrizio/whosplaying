b68ca5de1a10ea90201997a4d2893a14749d814f
Fabrizio Mele <fabriziomele92@gmail.com>
2016-02-19 15:07:07 +0100

    dynamic response

--- webhook.php
+++ webhook.php
@@ -1,11 +1,17 @@
 <?php 
 define('__ROOT__', __DIR__); 
 require_once  __ROOT__.'/conf.php';
+require_once __ROOT__.'/apiServer.php'
 // read incoming info and grab the chatID
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 $chatID = $update["message"]["chat"]["id"];
-		
+$messageReceived = $update['message']['text'];
+
+if($messageReceived == "/pippo"){
+	$mode = new Modello();
+	sendMessage($mod->getFollowing("pippo"));
+}
 // compose reply
 $reply =  sendMessage();
 		
@@ -13,8 +19,8 @@ $reply =  sendMessage();
 $sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
 file_get_contents($sendto);
 
-function sendMessage(){
-$message = "I am a baby bot.";
+function sendMessage($str){
+$message = $str;
 return $message;
 }
 
